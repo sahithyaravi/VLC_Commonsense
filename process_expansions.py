ef712cbfb2a6834a29b9d079ace04f1695224f28
sahithya <sahithyaravi1493@gmail.com>
2022-02-11 14:58:38 -0800

    more edits

--- process_expansions.py
+++ process_expansions.py
@@ -82,7 +82,7 @@ def expansions_to_sentences(expansions, sentences, save_path, topk_path, srl=Fal
         all_top_contexts = dict(zip(keys, top_contexts))
         save_json(save_path, all_contexts)
         save_json(topk_path, all_top_contexts)
-    print(all_contexts)
+    # print(all_contexts)
     return all_contexts, all_top_contexts
 
 
@@ -95,7 +95,7 @@ def search_caption_expansions(caption_expanded, questions_df, parallel=False):
             queries = list(df_img['question'].values)
             qids = list(df_img['question_id'].values)
             # picked, _ = symmetric_search(queries, context, k=10, threshold=0.01)
-            img_text, text_only = image_symmetric_search(img_path, queries, context, k=30, threshold=0)
+            img_text, text_only = image_symmetric_search(img_path, queries, context, k=15, threshold=0)
             picked_img = dict(zip(qids, img_text))
             picked_text = dict(zip(qids, picked_text))
         return picked_img, picked_text
@@ -103,14 +103,16 @@ def search_caption_expansions(caption_expanded, questions_df, parallel=False):
     # we only need to process those images that have questions:
     question_image_ids = set(questions_df["image_path"].unique())
     caption_keys = set(caption_expanded.keys())
-    img_paths = list(question_image_ids & caption_keys)[:10]
+    img_paths = list(question_image_ids & caption_keys)
+    img_paths.sort()
+    img_paths = img_paths[:50]
     img_ids = [image_path_to_id(key) for key in img_paths]
 
     if parallel:
-        final_list_img, final_list = Parallel(n_jobs=-1)(
-            delayed(semantic_search_job)(img_paths[i], caption_expanded[img_paths[i]], questions_df) for i in tqdm(range(len(img_paths))))
+        final_list_img, final_list = zip(*Parallel(n_jobs=-1)(
+            delayed(semantic_search_job)(img_paths[i], caption_expanded[img_paths[i]], questions_df) for i in tqdm(range(len(img_paths)))))
         final_context = dict(zip(img_ids, final_list))
-        final_context = dict(zip(img_ids, final_list_img))
+        final_context_img = dict(zip(img_ids, final_list_img))
     else:
         final_context_img = {}
         final_context = {}
@@ -200,4 +202,25 @@ if __name__ == '__main__':
     caption_expansions_sentences, top_caption_expansions_sentences = expansions_to_sentences(caption_expansions,
                                 captions, caption_expansion_sentences_path, topk_caption_path, parallel=True)
 
+    logger.info(f"Starting to pick final expansions using {method}:")
+    if method == "Sem_V1":
+        out, out1 = search_caption_expansions(caption_expansions_sentences, df, parallel=False)
 
+    elif method == "Sem_V2":
+        question_expansions = load_json(questions_comet_expansions_path)
+        question_expansions_sentences = expansions_to_sentences(question_expansions,
+                                questions, question_expansion_sentences_path, topk_qn_path, parallel=False)
+        out, out1 = search_caption_qn_expansions(question_expansions_sentences,
+                                                             caption_expansions_sentences, df)
+    else:
+        logger.warning("You are not using semantic search. Picking using topk expansions")
+        if os.path.exists(topk_qn_path):
+            top_question_expansions_sentences = load_json(topk_qn_path)
+        if os.path.exists(topk_caption_path):
+            top_caption_expansions_sentences = load_json(topk_caption_path)
+
+        out, out1 = pick_expansions_method_top(top_question_expansions_sentences,
+                                                           top_caption_expansions_sentences,
+                                                           df)
+    save_json(final_expansion_save_path + "I.json", out)
+    save_json(final_expansion_save_path + ".json", out1)
